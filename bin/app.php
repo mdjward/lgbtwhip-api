5a22d4c3cc34268c26633bd8e7460486a8664700
Matt Ward <matthew.ward@byng-systems.com>
2015-02-03 20:58:13 +0000

    Added project skeleton.

---
+++ bin/app.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * app.php
+ * 
+ * Created 03-Feb-2015 20:09:03
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ */
+
+require_once __DIR__ . '/../var/bootstrap.php';
---
+++ var/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * bootstrap.php
+ * 
+ * Created 03-Feb-2015 20:11:49
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
