d1d75d521d489c27a6be9cd3d015edea4aa6d2a9
Hereward Mills <hereward.mills@softwire.com>
2015-03-01 18:48:56 +0000

    Alter find by postcode to use ConstituencyRepository.

--- test/TheLgbtWhip/Api/Tests/Controller/ConstituencyControllerTest.php
+++ test/TheLgbtWhip/Api/Tests/Controller/ConstituencyControllerTest.php
@@ -5,6 +5,8 @@ namespace TheLgbtWhip\Api\Tests\Controller;
 use Phockito;
 use PHPUnit_Framework_TestCase as TestCase;
 use TheLgbtWhip\Api\Controller\ConstituencyController;
+use TheLgbtWhip\Api\Model\View\Constituency;
+use TheLgbtWhip\Api\Repository\ConstituencyRepository;
 use TheLgbtWhip\Api\External\Client\MapItClient;
 
 
@@ -20,6 +22,7 @@ class ConstituencyControllerTest extends TestCase
      */
     const TEST_POSTCODE = "TestCode";
     const TEST_NAME = "Test Name";
+    const TEST_ID = 123;
 
     /**
      *
@@ -32,42 +35,51 @@ class ConstituencyControllerTest extends TestCase
      * @var ConstituencyController 
      */
     private $controller;
-    
-    
-    
+
+    /**
+     * @var ConstituencyRepository
+     */
+    private $constituencyRepository;
+
+    /**
+     * @var Constituency
+     */
+    private $constituency;
+
+
     /**
      * 
      */
     protected function setUp()
     {
-        $this->client = Phockito::mock('TheLgbtWhip\Api\External\Client\MapItClient');
-        $this->controller = new ConstituencyController($this->client);
+        $this->_mockConstituency();
+        $this->client = mock('TheLgbtWhip\Api\External\Client\MapItClient');
+        $this->constituencyRepository = mock('TheLgbtWhip\Api\Repository\ConstituencyRepository');
+        $this->controller = new ConstituencyController($this->client, $this->constituencyRepository);
     }
-    
+
     /**
      * @test
      */
-    public function testResolvePostcodeReturnsConstituency()
+    public function testResolveByPostcodeSetsConstituencyName()
     {
+        // given
+        when($this->client->getConstituencyFromPostcode(self::TEST_POSTCODE))->return(self::TEST_ID);
+        when($this->constituencyRepository->find(self::TEST_ID))->return($this->constituency);
+
         // when
         $result = $this->controller->resolveByPostcodeAction(self::TEST_POSTCODE);
 
         // then
         $this->assertInstanceOf('TheLgbtWhip\Api\Model\View\Constituency', $result);
+        $this->assertEquals(self::TEST_ID, $result->id);
+        $this->assertEquals(self::TEST_NAME, $result->name);
     }
 
-    /**
-     * @test
-     */
-    public function testResolveByPostcodeGetsNameFromMapit()
+    private function _mockConstituency()
     {
-        // given
-        when($this->client->getConstituencyFromPostcode(self::TEST_POSTCODE))->return(self::TEST_NAME);
-
-        // when
-        $result = $this->controller->resolveByPostcodeAction(self::TEST_POSTCODE);
-
-        // then
-        $this->assertEquals(self::TEST_NAME, $result->name);
+        $this->constituency = new Constituency();
+        $this->constituency->id = self::TEST_ID;
+        $this->constituency->name = self::TEST_NAME;
     }
 }
