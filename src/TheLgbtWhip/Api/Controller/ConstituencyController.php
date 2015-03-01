c71e52d6294b3483bed09698bd531fe76a06ab89
Matt Ward <matthew.ward@byng-systems.com>
2015-03-01 13:35:20 +0000

    Added sample code to illustrate the planned project structure.

---
+++ src/TheLgbtWhip/Api/Controller/ConstituencyController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * ConstituencyController.php
+ * Definition of class ConstituencyController
+ * 
+ * Created 01-Mar-2015 13:28:18
+ *
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ * @copyright (c) 2015, Byng Systems Ltd
+ */
+namespace TheLgbtWhip\Api\Controller;
+
+
+
+/**
+ * ConstituencyController
+ * 
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ */
+class ConstituencyController
+{
+    
+    public function resolveByPostcodeAction($givenPostcode)
+    {
+        
+    }
+    
+}
---
+++ src/TheLgbtWhip/Api/External/Client/MapItClient.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * MapItClient.php
+ * Definition of class MapItClient
+ * 
+ * Created 01-Mar-2015 13:29:32
+ *
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ * @copyright (c) 2015, Byng Systems Ltd
+ */
+namespace TheLgbtWhip\Api\External\Client;
+
+
+
+/**
+ * MapItClient
+ * 
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ */
+class MapItClient
+{
+    
+    
+    
+}
