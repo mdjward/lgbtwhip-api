ba473e701d79cf57360dc4faf708c53a8988e8f7
Hereward Mills <hereward.mills@softwire.com>
2015-03-01 19:19:41 +0000

    Return 404 if constituency not found.
    Conflicts:
    	etc/di_container/controller.yml

--- src/TheLgbtWhip/Api/Controller/ConstituencyController.php
+++ src/TheLgbtWhip/Api/Controller/ConstituencyController.php
@@ -2,7 +2,7 @@
 /**
  * ConstituencyController.php
  * Definition of class ConstituencyController
- * 
+ *
  * Created 01-Mar-2015 13:28:18
  *
  * @author M.D.Ward <matthew.ward@byng-systems.com>
@@ -10,6 +10,7 @@
  */
 namespace TheLgbtWhip\Api\Controller;
 
+use Slim\Http\Response;
 use TheLgbtWhip\Api\External\Client\MapItClient;
 use TheLgbtWhip\Api\Model\View\Constituency;
 use TheLgbtWhip\Api\Repository\ConstituencyRepository;
@@ -17,12 +18,12 @@ use TheLgbtWhip\Api\Repository\ConstituencyRepository;
 
 /**
  * ConstituencyController
- * 
+ *
  * @author M.D.Ward <matthew.ward@byng-systems.com>
  */
 class ConstituencyController
 {
-    
+
     /**
      *
      * @var MapItClient
@@ -33,13 +34,20 @@ class ConstituencyController
      * @var ConstituencyRepository
      */
     private $constituencyRepository;
+    /**
+     * @var \Slim\Http\Response
+     */
+    private $response;
 
 
     /**
+     * @param \Slim\Http\Response $response
      * @param MapItClient $mapItClient
      * @param \TheLgbtWhip\Api\Repository\ConstituencyRepository $constituencyRepository
      */
-    public function __construct(MapItClient $mapItClient, ConstituencyRepository $constituencyRepository) {
+    public function __construct(Response $response, MapItClient $mapItClient, ConstituencyRepository $constituencyRepository)
+    {
+        $this->response = $response;
         $this->mapItClient = $mapItClient;
         $this->constituencyRepository = $constituencyRepository;
     }
@@ -51,7 +59,13 @@ class ConstituencyController
     public function resolveByPostcodeAction($givenPostcode)
     {
         $id = $this->mapItClient->getConstituencyFromPostcode($givenPostcode);
-        return $this->constituencyRepository->find($id);
+        $constituency = $this->constituencyRepository->find($id);
+        if (is_null($constituency)) {
+            $this->response->setStatus(404);
+            return null;
+        } else {
+            return $constituency;
+        }
     }
-    
+
 }
