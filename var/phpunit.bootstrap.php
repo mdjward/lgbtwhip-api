892d0f82db000898e797939da1d6e5df2c7da429
Matt Ward <matthew.ward@byng-systems.com>
2015-03-01 16:11:28 +0000

    Introduced test bootstrap file to automatically include hamcrest on test initiation.

---
+++ var/phpunit.bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * phpunit.bootstrap.php
+ * 
+ * Created 01-Mar-2015 16:08:49
+ *
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ * @copyright (c) 2015, Byng Systems Ltd
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+Phockito::include_hamcrest();
