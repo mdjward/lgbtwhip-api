41dabfc2ccf1cb80ba5cba3f2575aa615a6be5ac
Hereward Mills <hereward.mills@softwire.com>
2015-03-07 14:03:28 +0000

    Add getCandidatesForConstituency method.

--- src/TheLgbtWhip/Api/External/Client/YourNextMpClient.php
+++ src/TheLgbtWhip/Api/External/Client/YourNextMpClient.php
@@ -1,23 +1,11 @@
 <?php
-/**
- * YourNextMpClient.php
- * Definition of class YourNextMpClient
- * 
- * Created 01-Mar-2015 15:00:15
- *
- * @author M.D.Ward <matthew.ward@byng-systems.com>
- * @copyright (c) 2015, Byng Systems Ltd
- */
+
 namespace TheLgbtWhip\Api\External\Client;
 
-
-
-/**
- * YourNextMpClient
- * 
- * @author M.D.Ward <matthew.ward@byng-systems.com>
- */
 class YourNextMpClient extends AbstractRestServiceClient
 {
-    
+    public function getCandidatesForConstituency($id)
+    {
+        return array();
+    }
 }
