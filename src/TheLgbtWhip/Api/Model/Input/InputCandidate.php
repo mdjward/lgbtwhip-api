bd9e594fe5368ef19e29ce119e86ba61be58f891
Matt Ward <matthew.ward@byng-systems.com>
2015-04-23 01:53:20 +0100

    Post-merge commits.

---
+++ src/TheLgbtWhip/Api/Model/Input/InputCandidate.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * InputCandidate.php
+ * Definition of class InputCandidate
+ * 
+ * Created 21-Apr-2015 07:55:42
+ *
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ * @copyright (c) 2015, Byng Systems Ltd
+ */
+namespace TheLgbtWhip\Api\Model\Input;
+
+
+
+/**
+ * InputCandidate
+ * 
+ * @author M.D.Ward <matthew.ward@byng-systems.com>
+ */
+class InputCandidate
+{
+    
+    
+    
+}
